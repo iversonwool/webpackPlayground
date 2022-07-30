eb6e57ec46d7fde3ab2fc0550fcee1b0b1f1a2cb
LeeHow <iversonwoool@gmail.com>
2022-07-31 06:36:47 +0800

    feat: 文件名称占位符

--- playground/webpack.config.js
+++ playground/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   // entry: './src/main.css',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   mode: 'none',
